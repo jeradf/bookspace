2dce863a2a808eaa2fdde86a722a86227cdf5750
jeradf <jeradfields@gmail.com>
2016-08-16 16:43:18 -0400

    fix bad view name

--- bookspace.py
+++ bookspace.py
@@ -99,7 +99,7 @@ def index():
     return render_template('index.html', items={}, query={})
 
 @app.route('/about/')
-def index():
+def about():
     return render_template('about.html')    
 
 @app.route('/search/', methods=['GET'])
