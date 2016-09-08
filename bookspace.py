8c011e9c0fa95e886bdb8b830ead62211819aede
jeradf <jeradfields@gmail.com>
2016-09-08 16:20:12 -0400

    moviespace

--- bookspace.py
+++ bookspace.py
@@ -1,74 +1,9 @@
+from pprint import pprint
 from flask import Flask, render_template, request, jsonify
-from gensim import matutils
-import time
-from collections import OrderedDict
-import numpy as np
-from numpy import prod, dot
 import json
-from gensim.models import Doc2Vec, Phrases
-from gensim.matutils import unitvec
-import cPickle as pickle
 from bisect import bisect_left
-import os
-import sys
-from book_search import title_search
-from gensim.utils import any2unicode
 import app_settings as settings
-root = settings.root_path
-home_dir = os.path.expanduser('~')
-if sys.platform=='darwin':
-    root = root.replace(home_dir, home_dir+'/Dropbox')
-
-model = Doc2Vec.load(home_dir+"/model/corpra/1M_d2v_trained_with_review_docs_and_related_docs_2")
-bigram = Phrases.load(root+'model/amz_bigram_aug6_130kbooks.p','rb')
-book_data = pickle.load( open(root+"model/book_meta_data.p", "rb" ) )
-title2asin = pickle.load( open(root+"model/title2asin_select.p", "rb" ) )
-
-def get_book_data(asins):
-    items = OrderedDict()
-    for i, asin in enumerate(asins):
-        if asin in book_data:
-            book = book_data[asin]
-            item = {'id': 'book_img' +str(i),
-                    'img_link': book['imUrl'],
-                    'buy_link': book['buy_link'],
-                    'description': '' }        
-            words = book['description'].split()
-            item['description'] = ' '.join( words[:min(60, len(words))]) +  " ... "
-            items[i] = item
-            if len(items)>=42:
-                break
-    return items
-
-def sim(query_book, pos_words, neg_words, topn=20):
-    pos_vecs = []
-    all_query_words = []
-    if query_book in title2asin:
-        all_query_words.append(query_book)
-        all_query_words.append(title2asin[query_book])
-        pos_vecs.append(model.docvecs[title2asin[query_book]])
-
-    for word in bigram[pos_words.replace(',', ' ').lower().split()]:
-        if word in model:            
-            all_query_words.append( word )
-            pos_vecs.append( model[word] )
-
-    neg_vecs = []
-    for word in bigram[neg_words.replace(',', ' ').lower().split()]:
-        if word in model:
-            all_query_words.append( word )
-            neg_vecs.append( model[word] )
-
-    if not pos_vecs:
-        print "No positive vecs found. Book: %s\nPos_words: %s"%(query_book, pos_words)
-        return None
-
-    print "All query words: \n\t%s" %'\n\t'.join(all_query_words)
-    asins, distances = zip(*model.docvecs.most_similar(positive=pos_vecs, 
-                                                       negative=neg_vecs,
-                                                       topn=100))
-    asins = filter(lambda asin: asin not in all_query_words, asins)    
-    return asins[:min(topn,len(asins)) ]
+from movie_space import title_search, get_similar, title2asin
 
 
 app = Flask(__name__)
@@ -81,13 +16,19 @@ def about():
     return render_template('about.html')    
 
 @app.route('/search/', methods=['GET'])
-def get_similar():
-    book = request.args.get('query_book', '', type=str)
-    print 'Query book: "%s"'%book
-    if book and book not in title2asin:
+def search():
+    books = request.args.get('query_book', '', type=str)    
+    print 'Query book: "%s"'%books.split(',')
+    
+    book_query = []
+    for book in books.split(','):
+        if book.strip() in title2asin:
+            book_query.append(book.strip())
+    
+    if books and not book_query:
         print "Book not found"
         query_params, items = {}, {}
-        return render_template('index.html', items=items, query=query_params)
+        return render_template('index.html', items={}, query={})
     
     pos_words = request.args.get('plus', '', type=str)    
     neg_words = request.args.get('minus', '', type=str)    
@@ -96,14 +37,16 @@ def get_similar():
         query_params,items = {}, {}
         return render_template('index.html', items=items, query=query_params)
 
-    query_params = {'book':book, 'pos':pos_words, 'neg':neg_words}
+    query_params = {'book': ', '.join(book_query), 'pos':pos_words, 'neg':neg_words}
     
     try:
-        asins = sim(book, pos_words, neg_words, topn=52)
-        items = get_book_data(asins) if asins else OrderedDict()
+        items = get_similar(book_query, pos_words, neg_words, topn=100)        
+        
     except Exception as e:
-        print "Exception in get_similar %s"%e
-        query_params = {}
+        print "Exception in get_similar %s"%e.message
+        print "\tReason: %s"%e.reason
+        print "\tStart: %s, End: %s"%(e.start,e.end)
+        return render_template('index.html', items={}, query={})
     return render_template('index.html', items=items, query=query_params)
 
 @app.route('/suggest',methods=['GET'])
@@ -118,9 +61,6 @@ def suggest():
 
 
 if __name__ == '__main__':
-    app.run(
-        host='0.0.0.0',
-        port=int("8000"),
-        debug=False
-    )
-
+    app.run(host='0.0.0.0',
+            port=int("8000"),
+            debug=0)
---
+++ movie_space.py
@@ -0,0 +1,154 @@
+from __future__ import division
+from pprint import pprint
+import cPickle as pickle
+import os
+import app_settings as settings
+import sys
+
+import time
+from collections import defaultdict, Counter, OrderedDict
+import string 
+import re
+import numpy as np
+from numpy import prod, dot
+from gensim.models import Doc2Vec, Phrases
+root = settings.root_path
+big_file_dir = os.path.expanduser('~')+'/model/corpra/'
+if sys.platform=='darwin':
+    root = root.replace(os.path.expanduser('~'),
+                        os.path.expanduser('~')+'/Dropbox')
+
+########################################################################
+# Find nearest neighbors in product space
+#######################################################################
+model = Doc2Vec.load(root+"model/movie_space/idf_reddit")
+bigram = Phrases.load(big_file_dir+'movies_bigram_large.p','rb')
+book_data = pickle.load( open(root+"model/movie_space/book_meta_data.p", "rb" ) )
+title2asin = pickle.load( open(root+"model/movie_space/title2asin.p", "rb" ) )
+
+def get_similar(query_book, pos_words, neg_words, topn=100):
+    try:
+        pos_vecs = []
+        all_query_words = []
+        for book in query_book:
+            if book in title2asin:
+                print "\tFound book: ", title2asin[book]            
+                all_query_words.append(title2asin[book])
+                pos_vecs.append(model.docvecs[title2asin[book]])
+
+        for word in bigram[pos_words.replace(',', ' ').lower().split()]:
+            if word in model:
+                print "\tPositive word: %s"%word
+                all_query_words.append( word )
+                pos_vecs.append( model[word] )
+
+        neg_vecs = []
+        for word in bigram[neg_words.replace(',', ' ').lower().split()]:
+            if word in model:
+                print "\tNegative word: %s"%word
+                all_query_words.append( word )
+                neg_vecs.append( model[word] )
+
+        if not pos_vecs:
+            print "No positive vecs found. Book: %s\nPos_words: %s"%(query_book, pos_words)
+            return None
+        else:
+            print "\tGot a total of %d pos_vecs and %d neg_vecs"%(len(pos_vecs),len(neg_vecs))
+
+        print "\tAll query words:\n\t %s"%('\n\t'.join(all_query_words))
+        asins, distances = zip(*model.docvecs.most_similar(positive=pos_vecs, 
+                                                           negative=neg_vecs,
+                                                           topn=300))
+        asins = filter(lambda asin: asin not in all_query_words and asin in book_data ,
+                        asins)
+
+        items = get_book_data(asins[:min(100,len(asins))],
+                              topn=100) if asins else OrderedDict()
+        print "got %d items"%len(items)
+    except Exception as e:
+        print "Exception in sim:\n\t%s"%e
+        return None
+    return items
+
+def movie_string(data):
+    try:
+        s = u'Title: {title}\nIMDB Rating: {rating:.1f}\n'    
+        s += u'Cast: {actors}\nPlot: {plot:.120s}...'
+        s = s.format(**data).replace('\n','<br>')
+    except Exception as e:
+        print "exception at in movie_string: %s"%e
+        return ""
+    return s
+
+def get_book_data(asins,topn):
+    items = OrderedDict()
+    for i, asin in enumerate(asins):
+        if asin in book_data:
+            book = book_data[asin]
+            if book['rating']>6.5:
+                item = {'id': 'book_img' +str(i),
+                        'img_link': book['imUrl'],
+                        'buy_link': book['buy_link'],
+                        'description': movie_string(book)}
+                words = book['description'].split()
+                # item['description'] = ' '.join( words[:min(60, len(words))]) +  " ... "
+                items[i] = item
+            if len(items)>=topn:
+                break
+    return items
+
+########################################################################
+# Movie title autocomplete functions
+########################################################################
+
+title_data = pickle.load( open(root+"model/movie_space/title_data.p", "rb" ) )
+titles = sorted(title_data.keys())
+
+RE_PUNCT_ALL = re.compile('([%s])+' % re.escape(string.punctuation ), re.UNICODE)
+
+def t2w(t):
+    return RE_PUNCT_ALL.sub(" ", t.lower() ).split()
+
+def make_word2title_hash(titles):
+    w2t = defaultdict(list)
+    for title in titles:
+        for word in t2w(title):
+            w2t[word].append(title)
+    return {w:list(set(t)) for w,t in w2t.iteritems()}
+
+w2t = make_word2title_hash(titles)
+
+def match_score(words_in_query, title, title_data=title_data):
+    words_in_title = title_data[title]['title_words']
+    common_words = len(words_in_query.intersection(words_in_title))
+    total_words = len(words_in_title.union(words_in_query))
+    n_reviews = title_data[title]['num_reviews']
+    score = n_reviews * int(100*(common_words/total_words))/100
+    return score
+
+def title_search(query):
+    words_in_query = set(t2w(query))
+    matches = []
+    for word in words_in_query:
+        if word in w2t:
+            matches.extend(w2t[word])
+
+    if not matches:
+        return None
+    
+    matches = Counter(matches)    
+    N = max(matches.values())
+    matched_titles = [title for title, count in matches.iteritems()
+                      if count==N]                
+    # Score each candidate by fraction of common words in all
+    # words times the number of reviews
+    scores = [match_score(words_in_query,title) 
+              for title in matched_titles]                
+    
+    scored_titles = sorted( zip(matched_titles, scores),
+                           key=lambda item: item[1],
+                           reverse=True);        
+    
+    scored_titles = scored_titles[:min(len(scored_titles),20)]
+    suggestions = list(zip(*scored_titles)[0])
+    return suggestions
